7d9377e37f5ffe8cccbaa81be60ac491900059af
sunpia <darm.hanyusun@gmail.com>
2019-07-10 23:16:57 +0200

    In task1 I cannot find outputT1 , so i use outputT2 to replace it

---
+++ HW03/task1.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "output.h"
+//In this programm I use linklist to store the string, so it can more than 256 characters
+typedef struct savechar{
+	struct savechar *next;
+	char str[10];
+	bool full;
+}sav;
+sav *read(int *number){
+	sav *head, *p;
+	p = (sav*)malloc(sizeof(sav));
+	p -> full = false;
+	p -> next = NULL;
+	head = p;
+	char c;
+	int num = 0;
+	while(1){
+		system("stty raw");
+		char c;
+		c = getchar();
+		if(c == '\r'){
+			break;
+		}else{	
+			if(num%10==0 && num != 0){
+				p->full = true;
+				p->next = (sav*)malloc(sizeof(sav));
+				p=p->next;
+				p->next =NULL;
+				p->full = false;
+			}
+			p->str[num%10] = c;
+		}
+		num++;
+		system("stty cooked");
+	}
+	printf("\n");
+	*number = num;
+	return head;
+}
+void delet(sav *head){
+	sav *p = head->next;
+	free(head);
+	if(p){
+	delet(p);
+	}
+}
+void main() {
+	int num;
+	sav *head = read(&num);
+	outputT2(num);
+	delet(head);		
+}
